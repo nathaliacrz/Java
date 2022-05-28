42148773ec7c9b7ca36a0a8a97ec63016ad36db2
nathaliacrz <nathalias161@gmail.com>
2022-05-28 12:48:35 -0300

    Exercicio conversão de temperatura concluido

--- src/fundamentos/Temperatura.java
+++ src/fundamentos/Temperatura.java
@@ -1,5 +1,24 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class Temperatura {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		double C, F;
+
+		System.out.println("Digite o valor em Fahrenheit: ");
+		F = sc.nextDouble();
+
+		C = (F - 32) * 5.0 / 9.0;
+
+		System.out.println("O valor em Celsius é de: " + C + "°C.");
+		
+		sc.close();
+	}
 }
+
+
